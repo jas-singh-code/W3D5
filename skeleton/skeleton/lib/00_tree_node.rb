6314cbba45c8cf05a60e860303868d2bae223d4f
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-01-15 14:00:10 -0500

    created class vars for PolyTree

--- skeleton/skeleton/lib/00_tree_node.rb
+++ skeleton/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,26 @@
 class PolyTreeNode
+    def initialize(value)
+        @parent= nil
+        @children = []
+        @value= value
+    end
 
+    def parent
+        @parent
+    end
+
+    def children
+        @children
+    end
+
+    def value
+        @value
+    end
+
+    def parent=(new_parent)
+        @parent= new_parent
+        @parent.children = 
+    end
+    
 end
+
