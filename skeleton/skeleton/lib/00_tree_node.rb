a8e5dec7214fbf07054dbfbbd4fc8922d8b34895
Abir Al Mahamud <abirmahamud610@gmail.com>
2021-01-15 14:26:38 -0500

    Working on parent setter

--- skeleton/skeleton/lib/00_tree_node.rb
+++ skeleton/skeleton/lib/00_tree_node.rb
@@ -18,8 +18,13 @@ class PolyTreeNode
     end
 
     def parent=(new_parent)
+        unless @parent.nil?
+            @parent.children.delete(@value)
+        end
         @parent= new_parent
-        @parent.children = 
+        unless @parent.children.include?(@value)
+            @parent.children << @value
+        end
     end
     
 end
